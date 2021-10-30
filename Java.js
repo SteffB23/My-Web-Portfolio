f5a2200f744a1dfa8cbae2f375154ec5572cc097
SteffB23 <sbrookens@hotmail.com>
2021-10-30 01:32:29 -0400

    working on appending images of projects

--- Java.js
+++ Java.js
@@ -1,1 +1,8 @@
-var gif = document.getElementById('project1')
+window.onload = function() {
+    var projectpic = document.createElement("img");
+    projectpic.setAttribute("src", "http://img.zohostatic.com/discussions/v1/images/defaultPhoto.png");
+    projectpic.setAttribute("height", "100px");
+    projectpic.setAttribute("width", "100px");
+    projectpic.setAttribute("alt", "Super Eats Project");
+    document.getElementsByClassName("card1").innerHTML = projectpic
+}
